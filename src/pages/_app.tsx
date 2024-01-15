226094f1a3d11f7549f37d5df9b0329d76cbcc45
aaolen <67482112+aaolen@users.noreply.github.com>
2024-01-14 21:40:28 -0500

    setup mantine theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import '@/styles/globals.css'
 import '@mantine/core/styles.css'
+import '@fontsource/baskervville';
 import type { AppProps } from 'next/app'
-import { createTheme, MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider, CSSVariablesResolver } from '@mantine/core';
 
-const theme = createTheme({})
+export const theme = createTheme({
+  primaryColor: 'violet',
+  headings: { fontFamily: 'Baskervville, Times New Roman, sans-serif', fontWeight: '400' },
+});
+
+export const resolver: CSSVariablesResolver = (theme) => ({
+  variables: { },
+  light: {
+    '--mantine-primary-color': 'var(--mantine-primary-color-9)',
+  },
+  dark: {
+    '--mantine-primary-color': 'var(--mantine-primary-color-6)',
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} cssVariablesResolver={resolver}>
       <Component {...pageProps} />
     </MantineProvider>
   ) 
