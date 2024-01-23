62f3fc13cc6b855d1c79f54926d19678dcea0254
aaolen <67482112+aaolen@users.noreply.github.com>
2024-01-23 15:19:27 -0500

    created dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,106 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ArrowLeft } from "lucide-react";
 
 export default async function Dashboard() {
   return (
-    <div className="flex min-h-screen justify-center items-center gap-8">
-      <p>Dashboard</p>
-      <ThemeToggle />
-    </div>
+    <>
+      <DashboardNavbar userEmail="test@gmail.com" />
+      <div className="mx-auto max-w-7xl px-12 pt-12">
+        <Tabs defaultValue="updates">
+          <TabsList>
+            <TabsTrigger value="updates">Updates</TabsTrigger>
+            <TabsTrigger value="reviews">Reviews</TabsTrigger>
+            <TabsTrigger value="about">About</TabsTrigger>
+          </TabsList>
+          <TabsContent value="updates">
+            <Card>
+              <CardHeader>
+                <CardTitle>Updates</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <UpdatesTabContent />
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="reviews">
+            <Card>
+              <CardHeader>
+                <CardTitle>Reviews</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <ReviewsTabContent />
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="about">
+            <Card>
+              <CardHeader>
+                <CardTitle>About</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <AboutTabContent />
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
+}
+
+function UpdatesTabContent() {
+  "use client";
+
+  return (
+    <>
+      <p>Updates content</p>
+    </>
   )
+}
+
+function ReviewsTabContent() {
+  "use client";
+
+  return (
+    <>
+      <p>Reviews content</p>
+    </>
+  )
+}
+
+function AboutTabContent() {
+  "use client";
+
+  return (
+    <>
+      <p>About content</p>
+    </>
+  )
+}
+
+async function DashboardNavbar(props: { userEmail: string }) {
+  return (
+    <>
+      <div className="border-border flex w-full items-center justify-center border-b p-2 px-4">
+        <div className="flex w-full max-w-7xl items-center justify-between">
+          <div className="flex items-center justify-center gap-4">
+            <Button size="sm" variant="outline">
+              <ArrowLeft className="h-4 w-4" />
+            </Button>
+            <p className="text-lg">Dashboard</p>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <p>{props.userEmail}</p>
+            <Button variant="outline" size="sm">
+              Logout
+            </Button>
+            <ThemeToggle size="sm" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
+import { Button, type ButtonProps } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,13 +12,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-export function ThemeToggle() {
+export function ThemeToggle(props: ButtonProps) {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" {...props}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
