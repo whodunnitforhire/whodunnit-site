99aa598b9e9658085c13485b63f42ff75091d1f4
aaolen <67482112+aaolen@users.noreply.github.com>
2024-02-06 16:16:17 -0500

    improved review dashboard ui

--- src/app/dashboard/_components/ReviewsTabContent.tsx
+++ src/app/dashboard/_components/ReviewsTabContent.tsx
@@ -6,7 +6,7 @@ import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { api } from "@/trpc/react";
 import { type RouterOutputs } from "@/trpc/shared";
-import { CalendarIcon, Edit, Loader2, ScanSearch } from "lucide-react";
+import { CalendarIcon, Edit, Loader2, Save } from "lucide-react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -21,16 +21,14 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { type ReactNode, useState } from "react";
 import { toast } from "sonner";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
+import TextCollapser from "@/components/TextCollapser";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { PopoverDialog, PopoverDialogContent, PopoverDialogTrigger } from "@/components/ui/popover-dialog";
 
 type ReviewsTabContentProps = {
   intialReviews: RouterOutputs["review"]["getAll"];
@@ -44,30 +42,14 @@ export default function ReviewsTabContent(props: ReviewsTabContentProps) {
     refetchOnWindowFocus: false,
   });
 
-  const [isEditing, setIsEditing] = useState(false);
-
   return (
     <div className="flex flex-col gap-4 pt-4">
       <div className="flex gap-4">
         <h1 className="text-3xl font-medium">Reviews</h1>
-        {isEditing ? (
-          <Button onClick={() => setIsEditing(false)}>
-            <ScanSearch className="h-6 w-6 pr-2" />
-            <span>View</span>
-          </Button>
-        ) : (
-          <Button onClick={() => setIsEditing(true)}>
-            <Edit className="h-6 w-6 pr-2" />
-            <span>Edit</span>
-          </Button>
-        )}
       </div>
       <Separator />
-      <div className="grid grid-cols-1 gap-4 pt-4 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 pt-4 pb-12 lg:grid-cols-3">
         {reviews?.map((review) => {
-          if (isEditing) {
-            return <ReviewEdit key={review.id} review={{ ...review }} />;
-          }
           return <ReviewView key={review.id} review={{ ...review }} />;
         })}
       </div>
@@ -81,11 +63,17 @@ function ReviewView(props: {
   const relativeTime = new RelativeTime();
   const date = relativeTime.from(props.review.datePosted)
   return (
-    <Card className="items-start space-y-2 p-6">
-      <p className="text-lg font-medium">{props.review.author}</p>
+    <Card className="relative items-start space-y-2 p-6">
+      <ReviewEditDialog review={props.review}>
+        <Button className="absolute right-6 top-6 z-50 space-x-2 shadow-md">
+          <Edit className="h-4 w-4" />
+          <span>Edit</span>
+        </Button>
+      </ReviewEditDialog>
+      <p className="text-lg font-medium leading-none">{props.review.author}</p>
       <p className="text-muted-foreground">{date}</p>
       <Rating count={props.review.rating} />
-      <p>{props.review.content}</p>
+      <TextCollapser value={props.review.content} maxChars={500} />
     </Card>
   );
 }
@@ -98,8 +86,9 @@ const reviewFormSchema = z.object({
 });
 
 // TODO: make user unable to switch back to view from edit while applying changes
-function ReviewEdit(props: {
+function ReviewEditDialog(props: {
   review: RouterOutputs["review"]["getAll"][number];
+  children: ReactNode;
 }) {
   const form = useForm<z.infer<typeof reviewFormSchema>>({
     resolver: zodResolver(reviewFormSchema),
@@ -112,6 +101,15 @@ function ReviewEdit(props: {
   });
 
   const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  function setOpenAndReset(toOpen: boolean) {
+    if (toOpen) {
+      form.reset({ ...props.review });
+    }
+    setOpen(() => toOpen);
+  }
+
 
   const utils = api.useUtils();
 
@@ -128,6 +126,7 @@ function ReviewEdit(props: {
     onSuccess: () => {
       void utils.review.getAll.invalidate();
       toast.success("Review updated.");
+      setOpen(false)
     },
   });
 
@@ -142,99 +141,105 @@ function ReviewEdit(props: {
   }
 
   return (
-    <Card className="p-4">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            control={form.control}
-            name="author"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Author</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="datePosted"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Date Posted</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-full pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground",
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      disabled={(date) =>
-                        date > new Date() || date < new Date("1900-01-01")
-                      }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="rating"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Rating</FormLabel>
-                <FormControl>
-                  <Input type="number" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="content"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Content</FormLabel>
-                <FormControl>
-                  <Textarea {...field} className="h-24 lg:h-72" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {isLoading ? (
-            <Button type="submit" disabled>
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              <span>Apply changes</span>
-            </Button>
-          ) : (
-            <Button type="submit">Apply changes</Button>
-          )}
-        </form>
-      </Form>
-    </Card>
+    <Dialog open={open} onOpenChange={setOpenAndReset}>
+      <DialogTrigger asChild>{props.children}</DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="author"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Author</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="datePosted"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Date Posted</FormLabel>
+                  <PopoverDialog>
+                    <PopoverDialogTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverDialogTrigger>
+                    <PopoverDialogContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverDialogContent>
+                  </PopoverDialog>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="rating"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Rating</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Content</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} className="h-24 lg:h-36" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Separator />
+            <div className="container flex items-center justify-center gap-4">
+              {isLoading ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <Button type="submit" disabled={!open}>
+                  <Save className="mr-2 h-4 w-4" />
+                  <span>Apply</span>
+                </Button>
+              )}
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
